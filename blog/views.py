547b0325495e0919b844ef858e4bc833d9119324
ejasquith <emily.asquith@icloud.com>
2022-12-11 04:22:25 +0000

    Display django messages to user

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 from .models import Post
 from .forms import CommentForm
 
@@ -46,6 +47,7 @@ class PostDetail(View):
             comment = comment_form.save(commit=False)
             comment.post = post
             comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Your comment has been submitted.')
         else:
             comment_form = CommentForm()
 
